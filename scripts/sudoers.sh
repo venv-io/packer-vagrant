f25e7c7f7cfde661d425887eb91a0e2f484029d2
liuning <liun@yun70.com.cn>
2015-01-21 17:30:50 +0800

    set nopasswd

--- scripts/sudoers.sh
+++ scripts/sudoers.sh
@@ -3,10 +3,10 @@
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
 sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
 
-/bin/cat << EOF > /etc/sudoers.d/wheel
-Defaults:%wheel env_keep += "SSH_AUTH_SOCK"
-Defaults:%wheel !requiretty
-%wheel ALL=(ALL) NOPASSWD: ALL
-EOF
-/bin/chmod 0440 /etc/sudoers.d/wheel
+#/bin/cat << EOF > /etc/sudoers.d/wheel
+#Defaults:%wheel env_keep += "SSH_AUTH_SOCK"
+#Defaults:%wheel !requiretty
+#%wheel ALL=(ALL) NOPASSWD: ALL
+#EOF
+#/bin/chmod 0440 /etc/sudoers.d/wheel
 
