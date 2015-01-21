f145738310c54091e36143605ddc5d5bfb4ebfd4
liuning <liun@yun70.com.cn>
2015-01-21 19:00:43 +0800

    updated

--- scripts/fix-slow-dns.sh
+++ scripts/fix-slow-dns.sh
@@ -13,8 +13,9 @@ if [[ "$PACKER_BUILDER_TYPE" == virtualbox* ]]; then
   /bin/echo '127.0.0.1   vagrant-centos-6.vagrantup.com' >> /etc/hosts
 
   echo insecure > /root/.curlrc
-  rpm -ivh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-  sed -i "s@https@http@g" `grep 'http' -rl /etc/yum.repos.d/`
+  #rpm -ivh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+  #sed -i "s@https@http@g" `grep 'http' -rl /etc/yum.repos.d/`
+  wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-6.repo
 
   # install libyaml
   /usr/bin/curl -L -o /tmp/yaml-0.1.4.tar.gz http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz
@@ -22,6 +23,17 @@ if [[ "$PACKER_BUILDER_TYPE" == virtualbox* ]]; then
   ./configure --prefix=/usr/local && make && make install
   cd /tmp/ && rm -rf yaml-0.1.4*
 
+  # 设置固定的yum source
+  sed -i "s@enabled=1@enabled=0@g" `grep 'enabled=1' -rl /etc/yum/pluginconf.d/fastestmirror.conf`
+  #sed -i "s@#baseurl=@baseurl=@g" `grep '#baseurl=' -rl /etc/yum.repos.d/epel.repo`
+  #sed -i "s@mirrorlist=@#mirrorlist=@g" `grep 'mirrorlist=' -rl /etc/yum.repos.d/epel.repo`
+
+  sed -i "s@mirrorlist=@#mirrorlist=@g" `grep 'mirrorlist=' -rl /etc/yum.repos.d/CentOS-Base.repo`
+
+  std_baseurl='#baseurl=http://mirror.centos.org/centos/$releasever/os/$basearch/'
+  new_baseurl='baseurl=http://mirrors.aliyun.com/centos/$releasever/os/$basearch/'
+  repo_file='/etc/yum.repos.d/CentOS-Base.repo'
+  sed -i "s@${std_baseurl}@${new_baseurl}@g" `grep '${std_baseurl}' -rl ${repo_file}`
 else
   echo 'Slow DNS fix not required for this platform, skipping'
 fi
